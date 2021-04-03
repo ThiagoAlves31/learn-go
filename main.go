a43e58b164e1ac0f6723b518d7f05b015cb13801
Thiago Alves <7564640-thiago_alves@users.noreply.gitlab.com>
2021-04-03 17:04:05 -0300

    incluindo funcao para buscar contas de um determinado cliente

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
+type Cliente struct {
+	Cliente          string     `json:"Cliente"`
+	CodigoSistemaXYZ string     `json:"CodigoSistemaXYZ"`
+	Contas           typeContas `json:"Contas"`
+}
+
+type typeContas []int
+
 func main() {
+
+	var clientes []Cliente
+
 	f, err := excelize.OpenFile("Exercicio.xlsx")
 
 	if err != nil {
@@ -15,5 +27,38 @@ func main() {
 	}
 
 	rows, err := f.GetRows("DePara")
-	fmt.Println(rows)
+
+	for nRow, row := range rows {
+
+		if nRow > 0 {
+			var nameClient = row[0]
+			var codeCliente = row[1]
+			result := searchClientContas(nameClient)
+			clientes = append(clientes, Cliente{Cliente: nameClient, CodigoSistemaXYZ: codeCliente, Contas: result})
+		}
+	}
+	fmt.Println(clientes)
+}
+
+func searchClientContas(codClient string) typeContas {
+
+	var contas typeContas
+
+	f, err := excelize.OpenFile("Exercicio.xlsx")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	rows, err := f.GetRows("Contas")
+	for _, row := range rows {
+
+		if row[0] == codClient {
+			int1, err := strconv.Atoi(row[1])
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			contas = append(contas, int(int1))
+		}
+	}
+	return contas
 }
